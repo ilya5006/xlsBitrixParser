10a38a96ad44f04baa0e1f8eb50eeba4dd73af05
ilya5006 <samarcev.ilya@mail.ru>
2019-09-25 09:19:18 +0300

    Interface

---
+++ interface.php
@@ -0,0 +1,4 @@
+<form method="POST" action="./load.php" enctype="multipart/form-data"> 
+    <input type="file" name="file" accept=".xls, .csv, .xlsx">
+    <input type="submit" name="accept">
+</form>
---
+++ load.php
@@ -0,0 +1,50 @@
+<?php
+    require_once './lib/PHPExcel/Classes/PHPExcel/IOFactory.php';
+    require_once './connection.php';
+
+    if (count($_FILES) == 1)
+    {
+        $file = $_FILES['file']['tmp_name'];
+    }
+
+    function isChanged($article, $quantity, $price)
+    {
+        global $link;
+
+        $queryResult = mysqli_query($link, "SELECT quantity, price FROM shop WHERE article = '$article'") or die('Произошла какая-то ошибка. Невозможно проверить данные');
+        $queryResult = mysqli_fetch_assoc($queryResult);
+        
+        if ($queryResult['quantity'] != $quantity || $queryResult['price'] != $price)
+            return true;
+        else
+            return false;
+    }
+
+    function changeValue($article, $quantity, $price)
+    {
+        global $link;
+        mysqli_query($link, "UPDATE shop SET quantity = '$quantity', price = '$price' WHERE article = '$article'") or die('Произогла какая-то ошибка. Обновить данные не удалось');
+    }
+
+    // Файл xlsx
+    //$xls = PHPExcel_IOFactory::load(__DIR__ . '/importExample.xlsx');
+    $xls = PHPExcel_IOFactory::load($file);
+
+    // Первый лист
+    $xls->setActiveSheetIndex(0);
+    $sheet = $xls->getActiveSheet();
+    
+    $row = $sheet->toArray();
+
+    for ($i = 1; $i < count($row); $i++)
+    {
+        list($article, $quantity, $price) = $row[$i];
+
+        echo '<pre>' . $article . ' : ' . $quantity . ' : ' . $price . '</pre>';
+
+        if (isChanged($article, $quantity, $price))
+        {
+            changeValue($article, $quantity, $price);
+        }
+    }
+?>
